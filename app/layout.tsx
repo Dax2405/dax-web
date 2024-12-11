48baecdb794839edcf656c90b08a67230770849e
Dax <daxnavarrete@gmail.com>
2024-12-11 11:09:29 -0500

    Remove unused font variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 const jetBrainsMono = localFont({
   src: "./fonts/JetBrainsMono-Regular.woff2",
   variable: "--font-jetbrains-mono",
